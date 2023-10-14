af6e8eb2e81e409ede771abd6d7db80e60aff4f5
Ash <coreyvladesov2023@u.northwestern.edu>
2023-10-13 23:07:42 -0500

    made professional ReadMe for project using my generator and placed it into my file

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -6,21 +6,38 @@ function renderLicenseBadge(license) {
   }
 }
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
 function renderLicenseLink(license) {
   if (license === "MIT") {
     return `[MIT License](https://opensource.org/licenses/MIT)`
   }
 }
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
 function renderLicenseSection(license) {
   if (license === "MIT") {
     return `This project is licensed under the [MIT License](https://opensource.org/licenses/MIT)`
   }
 }
+
+
+// Doing the same as the above for the other license option of Apache
+function renderLicenseBadge(license) {
+  if (license === "Apache") {
+    return `[![License Apache](https://img.shields.io/badge/License-Apache-yellow)](http://www.apache.org/licenses/)`
+  }
+}
+
+function renderLicenseLink(license) {
+  if (license === "Apache") {
+    return `[Apache License](http://www.apache.org/licenses/LICENSE-2.0)`
+  }
+}
+
+function renderLicenseSection(license) {
+  if (license === "Apache") {
+    return `This project is licensed under the [Apache License](http://www.apache.org/licenses/LICENSE-2.0)`
+  }
+}
+
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
